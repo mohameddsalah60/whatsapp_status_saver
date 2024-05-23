fb20f3b362c91e973f9afaa91b490eb06801468b
mohhamedsalahh <tktkyttttttttttk@gmail.com>
2024-05-23 13:56:02 +0300

    create Saved Tab Bar View

--- lib/views/widgets/home_screen_body.dart
+++ lib/views/widgets/home_screen_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'home_screen_header.dart';
 import 'image_tab_bar_view.dart';
+import 'saved_tab_bar_view.dart';
 import 'tab_bar_header.dart';
 import 'video_tab_bar_view.dart';
 
@@ -22,7 +23,7 @@ class HomeScreenBody extends StatelessWidget {
               children: [
                 ImageTabBarView(),
                 VideoTabBarView(),
-                Center(child: Text('Content of Tab Three')),
+                SavedTabBarView(),
               ],
             ),
           ),
---
+++ lib/views/widgets/saved_tab_bar_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SavedTabBarView extends StatelessWidget {
+  const SavedTabBarView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
